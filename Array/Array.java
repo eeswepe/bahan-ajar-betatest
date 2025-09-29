a1566d26a390a1a32dc2ac9421a04b2bd60bbbef
eeswepe <singgihwahyupermana@gmail.com>
2025-09-29 16:29:26 +0700

    initital commit

---
+++ Array/Array.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Array {
+
+  public static void main(String[] args) {
+    int [7] arr = {1,2,3,4,5,6 7};
+
+    for(int i=0;i<8;i++){
+      System.out.print(arr[i]);
+    }
+  }
+}
+
+// Output yang diharapkan adalah 1,2,3,4,5,6,7
+
---
+++ Function2/Function2.java
@@ -0,0 +1,15 @@
+package Function2;
+public class Function2 {
+
+  public String[] pangkatTiga(int n) {
+    return (n * n * n);
+  }
+
+  public static void main(String[] args) {
+    int angka = 2;
+    System.out.println(pangkaTiga(angka))
+  }
+}
+
+// Output yang diharapkan adalah hasil pangkat Tiga dari variabel angka
+// 8
