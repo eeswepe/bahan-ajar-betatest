36c68959c926b7eadbd56f467a8b7ea461113041
eeswepe <singgihwahyupermana@gmail.com>
2025-09-29 16:34:45 +0700

    feat: Array2 todo

--- Array/Array.java
+++ Array/Array.java
@@ -3,7 +3,7 @@ package Array;
 public class Array {
 
   public static void main(String[] args) {
-    int [7] arr = {1,2,3,4,5,6 7};
+    String [7] arr = {1,2,3,4,5,6 7};
 
     for(int i=0;i<8;i++){
       System.out.print(arr[i]);
